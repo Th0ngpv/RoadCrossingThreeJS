05a1f955d5941c2fd027b538c7908b0ede51a88a
Thong <117352680+Th0ngpv@users.noreply.github.com>
2025-06-07 22:55:43 +1000

    Create animateVehicles.js

---
+++ threejs-website/src/animateVehicles.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+import { metadata as rows } from './components/Map';
+import { minTileIndex, maxTileIndex, tileSize } from './constants';
+
+const clock = new THREE.Clock();
+
+export function animateVehicles() {
+    const delta = clock.getDelta(); // Get the time since the last frame
+
+    // animate cars and trucks
+    rows.forEach((rowData) => {
+        if (rowData.type === 'car' || rowData.type === 'truck') {
+
+            const beginningOfRow = (minTileIndex - 2) * tileSize;
+            const endOfRow = (maxTileIndex + 2) * tileSize;
+
+            rowData.vehicles.forEach(({ ref }) => {
+                if (!ref) throw Error('Vehicle reference is missing');
+
+                if (rowData.direction) {
+
+                    ref.position.x = 
+                        ref.position.x > endOfRow
+                        ? beginningOfRow
+                        : ref.position.x + (tileSize * delta);
+                } else {
+                    ref.position.x = 
+                        ref.position.x < beginningOfRow
+                        ? endOfRow
+                        : ref.position.x - (tileSize * delta);
+        }
+      });
+    }
+  });
+}
