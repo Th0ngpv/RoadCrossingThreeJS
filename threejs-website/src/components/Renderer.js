6863c2d9854b768773762a026001f0326167ff55
Thong <117352680+Th0ngpv@users.noreply.github.com>
2025-06-07 22:44:55 +1000

    Update Renderer.js

--- threejs-website/src/components/Renderer.js
+++ threejs-website/src/components/Renderer.js
@@ -1,23 +1,17 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 export function Renderer() {
+  const canvas = document.querySelector("canvas.game");
+  if (!canvas) throw new Error("Canvas not found");
 
-    // get the canvas element from the DOM
-    const canvas = document.querySelector("canvas.game");
-    if (!canvas) {
-        throw new Error("Canvas element not found");
-    }
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+    canvas: canvas,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
 
-    // Create a WebGL renderer
-    const renderer = new THREE.WebGLRenderer({
-        alpha: true,
-        antialias: true,
-        canvas: canvas,
-    });
-
-    // Set the pixel ratio and size of the renderer
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    return renderer;
+  return renderer;
 }
