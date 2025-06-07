62ff4aced2ac7131cf786faae8f94fb3c7c17698
Thong <117352680+Th0ngpv@users.noreply.github.com>
2025-06-07 23:38:19 +1000

    Create a new body for the player to handle the movement

--- threejs-website/src/components/Player.js
+++ threejs-website/src/components/Player.js
@@ -3,24 +3,54 @@ import * as THREE from 'three';
 export const player = Player();
 
 function Player() {
-    const body = new THREE.Mesh(
-        new THREE.BoxGeometry(15, 15, 20),
-        new THREE.MeshStandardMaterial({ 
-            color: "white",
-            flatShading: true,
-        })
-    );
-    body.position.z =10;
-    body.castShadow = true;
-    body.receiveShadow = true;
+  const player = new THREE.Group();
+ 
+  const body = new THREE.Mesh(
+    new THREE.BoxGeometry(15, 15, 20),
+    new THREE.MeshLambertMaterial({
+      color: "white",
+      flatShading: true,
+    })
+  );
+  body.castShadow = true;
+  body.receiveShadow = true;
+  body.position.z = 10;
+  player.add(body);
 
-    return body;
+  const cap = new THREE.Mesh(
+    new THREE.BoxGeometry(4, 6, 2),
+    new THREE.MeshLambertMaterial({
+      color: 0xf0619a,
+      flatShading: true,
+    })
+  );
+  cap.position.z = 21;
+  cap.castShadow = true;
+  cap.receiveShadow = true;
+  player.add(cap);
+
+  const peak = new THREE.Mesh(
+    new THREE.ConeGeometry(3, 5, 4),
+    new THREE.MeshLambertMaterial({
+      color: "yellow",
+      flatShading: true,
+    })
+  );  
+  peak.position.z = 15;
+    peak.position.y = 10;
+    peak.castShadow = true;
+    peak.receiveShadow = true;
+    player.add(peak);
+
+  return player;
 }
 
 export const position = {
     currentRow: 0,
     currentTile: 0,
 };
+
+export const movesQueue = [];
 
 export function queueMove(direction) {
   movesQueue.push(direction);
