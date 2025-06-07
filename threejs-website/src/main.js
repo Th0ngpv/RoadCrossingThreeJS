e273c6c4855413f5b567565fe5e17e3f853158da
Thong <117352680+Th0ngpv@users.noreply.github.com>
2025-06-07 23:38:26 +1000

    Add everything to main

--- threejs-website/src/main.js
+++ threejs-website/src/main.js
@@ -5,7 +5,9 @@ import { DirectionalLight } from "./components/DirectionalLight";
 import { player } from "./components/Player";
 import { map, initializeMap } from "./components/Map";
 import { animateVehicles } from "./animateVehicles.js";
+import { animatePlayer } from "./animatePlayer.js";
 import "./style.css";
+import "./collectUserInput";
 
 const scene = new THREE.Scene();
 scene.add(player);
@@ -31,5 +33,6 @@ renderer.setAnimationLoop(animate);
 
 function animate() {
   animateVehicles();
+  animatePlayer();
   renderer.render(scene, camera);
 }
