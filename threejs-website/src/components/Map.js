2718269dcd0568fd097f3a90097a015670779d2c
Thong <117352680+Th0ngpv@users.noreply.github.com>
2025-06-08 04:47:21 +1000

    modify the metadata handler

--- threejs-website/src/components/Map.js
+++ threejs-website/src/components/Map.js
@@ -1,50 +1,16 @@
 import * as THREE from "three";
+import { generateRows } from "../utilities/generateRows";
 import { Grass } from "./Grass";
 import { Road } from "./Road";
 import { Tree } from "./Tree";
 import { Car } from "./Car";
 import { Truck } from "./Truck";
 
-export const metadata = [
-  {
-    type: "car",
-    direction: false,
-    speed: 188,
-    vehicles: [
-      { initialTileIndex: -4, color: 0xbdb638 },
-      { initialTileIndex: -1, color: 0x78b14b },
-      { initialTileIndex: 4, color: 0xa52523 },
-    ],
-  },
-  {
-    type: "forest",
-    trees: [
-      { tileIndex: -5, height: 50 },
-      { tileIndex: 0, height: 30 },
-      { tileIndex: 3, height: 50 },
-    ],
-  },
-  {
-    type: "truck",
-    direction: true,
-    speed: 125,
-    vehicles: [
-      { initialTileIndex: -4, color: 0x78b14b },
-      { initialTileIndex: 0, color: 0xbdb638 },
-    ],
-  },
-  {
-    type: "forest",
-    trees: [
-      { tileIndex: -8, height: 30 },
-      { tileIndex: -3, height: 50 },
-      { tileIndex: 2, height: 30 },
-    ],
-  },
-];
+export const metadata = [];
 
 export const map = new THREE.Group();
 
+// Initialize the map with grass rows
 export function initializeMap() {
   for (let rowIndex = 0; rowIndex > -5; rowIndex--) {
     const grass = Grass(rowIndex);
@@ -53,51 +19,57 @@ export function initializeMap() {
   addRows();
 }
 
+// Generate and add new rows to the map
 export function addRows() {
-  metadata.forEach((rowData, index) => {
-    const rowIndex = index + 1;
+    const newMetadata = generateRows(20);
 
-    if (rowData.type === "forest") {
-      const row = Grass(rowIndex);
+    const startIndex = metadata.length;
+    metadata.push(...newMetadata);
+    
+    metadata.forEach((rowData, index) => {
+        const rowIndex = startIndex + index + 1;
 
-      rowData.trees.forEach(({ tileIndex, height }) => {
-        const three = Tree(tileIndex, height);
-        row.add(three);
-      });
+        if (rowData.type === "forest") {
+            const row = Grass(rowIndex);
 
-      map.add(row);
-    }
+            rowData.trees.forEach(({ tileIndex, height }) => {
+                const three = Tree(tileIndex, height);
+                row.add(three);
+            });
 
-    if (rowData.type === "car") {
-      const row = Road(rowIndex);
+            map.add(row);
+        }
 
-      rowData.vehicles.forEach((vehicle) => {
-        const car = Car(
-          vehicle.initialTileIndex,
-          rowData.direction,
-          vehicle.color
-        );
-        vehicle.ref = car;
-        row.add(car);
-      });
+        if (rowData.type === "car") {
+            const row = Road(rowIndex);
 
-      map.add(row);
-    }
+            rowData.vehicles.forEach((vehicle) => {
+                const car = Car(
+                vehicle.initialTileIndex,
+                rowData.direction,
+                vehicle.color
+                );
+                vehicle.ref = car;
+                row.add(car);
+            });
 
-    if (rowData.type === "truck") {
-      const row = Road(rowIndex);
+            map.add(row);
+        }
 
-      rowData.vehicles.forEach((vehicle) => {
-        const truck = Truck(
-          vehicle.initialTileIndex,
-          rowData.direction,
-          vehicle.color
-        );
-        vehicle.ref = truck;
-        row.add(truck);
-      });
+        if (rowData.type === "truck") {
+            const row = Road(rowIndex);
 
-      map.add(row);
-    }
+            rowData.vehicles.forEach((vehicle) => {
+                const truck = Truck(
+                vehicle.initialTileIndex,
+                rowData.direction,
+                vehicle.color
+                );
+                vehicle.ref = truck;
+                row.add(truck);
+            });
+
+            map.add(row);
+        }
   });
 }
